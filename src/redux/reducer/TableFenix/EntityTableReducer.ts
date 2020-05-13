dadc675085cf5215cb66c36df741306a5435fe44
trifenix <alejandro.iglesas@trifenix.io>
2020-05-13 00:11:02 -0400

    actualizado valor por defecto de tablas

--- src/redux/reducer/TableFenix/EntityTableReducer.ts
+++ src/redux/reducer/TableFenix/EntityTableReducer.ts
@@ -1,11 +1,11 @@
-
+import IEntityState from "./IEntityTableState";
 import { IResult } from "@fenix/tf-search-model";
 import {TableActionTypes, GET_TABLE_SUCCESS} from "../../actionTypes/TableFenix"
 
 
 
 
-export default function EntityTableReducer(state: any, action: TableActionTypes): any {
+export default function EntityTableReducer(state: any = {Entities: {}}, action: TableActionTypes): any {
   switch (action.type) {
     case GET_TABLE_SUCCESS:
       
